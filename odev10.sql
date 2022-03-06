61ed608820bf2f38913ad5866d812448617bf0b8
kiymet.toy-p <kiymet.toy-p@tubitak.gov.tr>
2022-03-06 20:57:04 +0300

    odev11

--- odev10.sql
+++ odev10.sql
@@ -1,13 +1,11 @@
--- 1. actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
-(SELECT first_name FROM actor) UNION (SELECT first_name FROM customer);
+-- 1. city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+SELECT city, country FROM city 
+LEFT JOIN country on city.country_id = country.country_id;
 
--- 2. actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
-(SELECT first_name from actor) INTERSECT (SELECT first_name from customer);
+-- 2. customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
 
--- 3. actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
-(SELECT first_name from actor) EXCEPT (SELECT first_name from customer);
-
--- 4. İlk 3 sorguyu tekrar eden veriler için de yapalım.
-(SELECT first_name from actor) UNION ALL (SELECT first_name from customer);
-(SELECT first_name from actor) INTERSECT ALL (SELECT first_name from customer);
-(SELECT first_name from actor) EXCEPT ALL (SELECT first_name from customer);
+-- 3. customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental
+FULL JOIN customer ON customer.customer_id = rental.customer_id;
